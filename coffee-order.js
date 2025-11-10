b58964209210e7bceb1669e12ed30df6e8f7de7d
Emils <emils@gmail.com>
2025-11-10 16:52:11 +0200

    pievienoju if..else

---
+++ coffee-order.js
@@ -0,0 +1,22 @@
+let coffeeSize=large;
+let addSugar=true;
+
+
+if (coffeeSize==large){
+    console.log("kafijas cena ir 2 EUR +20 centi cukura piedeva")
+}
+else if (coffeeSize==medium){
+    console.log("kafijas cena ir 1.5 EUR +20 centi cukura piedeva")
+}
+else {
+    console.log("kafijas cena ir 1 EUR +20 centi cukura piedeva")
+}
+
+
+
+
+
+
+
+
+
