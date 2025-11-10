4f566e7b713918357ea6a7fa6df0f973ec016075
Emils <emils@gmail.com>
2025-11-10 16:53:03 +0200

    pievienoju books-array tipa mainīgo

---
+++ books-array.js
@@ -0,0 +1,21 @@
+const books=[
+{title:"Kapteinis Apakšbiksis: ",author:"Deivs Pilkijs",isAvailable:true+" :ir pieejama"},
+{title:"Grega dienasgrāmata: ",author:"Džefs Kinnijs",isAvailable:false+" :nav pieejama"},
+]
+
+for (let i = 0 ; i < books.length; i ++){
+console.log("title:"+books[i].title+ books[i].isAvailable);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
